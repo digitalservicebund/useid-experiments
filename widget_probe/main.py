4469b595c90121d4113f654398489aa99cca5e48
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2023-05-09 13:50:08 +0200

    Include AusweisApp2 test

--- widget_probe/main.py
+++ widget_probe/main.py
@@ -5,5 +5,22 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("widget_probe.html")
+def landing_page():
+    return render_template("landing_page.html")
+
+@app.route("/timeout")
+def timeout():
+    return render_template("timeout_exec_break.html")
+
+@app.route("/not_installed")
+def not_installed():
+    return render_template("not_installed.html")
+
+@app.route("/what_apps_installed")
+def check_bundesident():
+    return render_template("check_bundesident.html")
+
+@app.route("/check_ausweisapp")
+def check_ausweisapp():
+    return render_template("check_ausweisapp.html")
+
