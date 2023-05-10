c4ae03a410736251bfb83b2d865ffd15fd54aede
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2023-05-10 15:14:47 +0200

    Clean up directory

--- widget_probe/main.py
+++ widget_probe/main.py
@@ -7,28 +7,15 @@ app = Flask(__name__)
 def landing_page():
     return render_template("landing_page.html")
 
-@app.route("/timeout")
-def timeout():
-    return render_template("timeout_exec_break.html")
-
 @app.route("/not_installed")
 def not_installed():
     return render_template("not_installed.html")
 
 @app.route("/what_apps_installed")
-def check_bundesident():
-    return render_template("check_bundesident.html")
-
-@app.route("/check_ausweisapp")
-def check_ausweisapp():
-    return render_template("check_ausweisapp.html")
-
-
-@app.route("/what_apps_installed/redirect")
 def check_bundesident_redirect():
     return render_template("check_bundesident_redirect.html")
 
-@app.route("/check_ausweisapp/redirect")
+@app.route("/check_ausweisapp")
 def check_ausweisapp_redirect():
     return render_template("check_ausweisapp_redirect.html")
 
